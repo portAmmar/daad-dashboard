8f228583e71c745005dcfe36bc48d0b14303e156
portAmmar <ammar9112004@gmail.com>
2025-10-06 23:49:18 +0300

    feat: add ar auth translations

---
+++ lang/ar/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'بيانات الدخول غير صحيحة.',
+    'password' => 'كلمة المرور التي أدخلتها غير صحيحة.',
+    'throttle' => 'محاولات تسجيل دخول كثيرة. يرجى المحاولة مرة أخرى بعد :seconds ثانية.',
+
+
+];
