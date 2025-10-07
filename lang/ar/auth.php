0e4819bb9fe031978bfdbcd0bcaef09c8b1c15a3
portAmmar <ammar9112004@gmail.com>
2025-10-07 06:01:20 +0300

    feat: tranlsate forget pass page

--- lang/ar/auth.php
+++ lang/ar/auth.php
@@ -18,6 +18,10 @@ return [
     'Password' => 'كلمة المرور',
     'Remember me' => 'ابقني مسجلا',
     'Forgot your password?' => 'هل نسيت كلمة المرور؟',
+    'Password Reset Link' => 'رابط استعادة كلمة المرور',
+    'Email Password Reset Link' => 'ارسل رابط إستعادة كلمة المرور',
+    'Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.' => 'هل نسيت كلمة المرور؟ لا داعي للقلق. قم بكتابه بريدك الألكتروني وسنرسل لك رابط استعادة كلمة المرور.',
+
 
     'failed' => 'بيانات الدخول غير صحيحة.',
     'password' => 'كلمة المرور التي أدخلتها غير صحيحة.',
