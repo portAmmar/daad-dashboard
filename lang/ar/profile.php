e27c47a15ac458e13992c39313df150fd8b62310
portAmmar <ammar9112004@gmail.com>
2025-10-09 20:58:31 +0300

    feat: trnsalte the profile feat components

---
+++ lang/ar/profile.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'delete_account' => 'حذف الحساب',
+    'delete_account_warning' => 'بمجرد حذف حسابك، ستُزال جميع بياناتك ومواردك بشكل نهائي. يُرجى تنزيل أي بيانات أو معلومات ترغب في الاحتفاظ بها قبل الحذف.',
+    'confirm_delete_account' => 'هل أنت متأكد أنك تريد حذف حسابك؟',
+    'confirm_delete_account_warning' => 'بعد حذف حسابك، سيتم مسح جميع بياناتك ومواردك نهائيًا. يُرجى إدخال كلمة المرور لتأكيد رغبتك في حذف الحساب بشكل دائم.',
+    'password' => 'كلمة المرور',
+    'cancel' => 'إلغاء',
+
+    'update_password' => 'تحديث كلمة المرور',
+    'password_security_hint' => 'تأكد من أن حسابك يستخدم كلمة مرور طويلة وعشوائية للحفاظ على أمانه.',
+    'current_password' => 'كلمة المرور الحالية',
+    'new_password' => 'كلمة المرور الجديدة',
+    'confirm_password' => 'تأكيد كلمة المرور',
+    'save' => 'حفظ',
+    'saved' => 'تم الحفظ.',
+
+    'profile_information' => 'معلومات الحساب',
+    'update_profile_info' => 'قم بتحديث معلومات حسابك الشخصية وعنوان بريدك الإلكتروني.',
+    'name' => 'الاسم',
+    'email' => 'البريد الإلكتروني',
+    'email_unverified' => 'عنوان بريدك الإلكتروني غير مُوثّق.',
+    'resend_verification' => 'اضغط هنا لإعادة إرسال رسالة التحقق.',
+    'verification_link_sent' => 'تم إرسال رابط تحقق جديد إلى بريدك الإلكتروني.',
+    'save' => 'حفظ',
+    'saved' => 'تم الحفظ.',
+
+    'profile' => 'الملف الشخصي',
+];
